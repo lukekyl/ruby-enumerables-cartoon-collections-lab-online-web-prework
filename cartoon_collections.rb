8d567495c4c1b69b0e38c1ffe52a98a56801c235
Kyle Luke <th.oldschool@gmail.com>
2019-08-01 23:38:52 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,5 +1,10 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(source_array)
+  answer={}
+  source_array.each_with_index{|n, index| 
+    answer[n] = index+1
+    puts "#{index+1}. #{n}"
+  }
+  answer
 end
 
 def summon_captain_planet# code an argument here
