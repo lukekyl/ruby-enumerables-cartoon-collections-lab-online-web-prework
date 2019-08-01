4e16e4fbd43050d39b544ab9abca83304afda90e
Kyle Luke <th.oldschool@gmail.com>
2019-08-01 23:55:27 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,8 +7,8 @@ def roll_call_dwarves(source_array)
   answer
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(palanteer_calls)
+  call = palanteer_calls.map{}
 end
 
 def long_planeteer_calls# code an argument here
